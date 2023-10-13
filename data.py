6a44c1a2b0f71bacaf018f00f0ff8438a37d23ea
Caio Monsores <caio.monsoress@gmail.com>
2023-10-12 21:08:48 -0300

    estilizando a pagina de personagem

--- data.py
+++ data.py
@@ -1,9 +1,56 @@
-personagens = [
-    {'nome': 'Principal', 'classe': 'Caminhão'},
-    {'nome': 'Vilão', 'classe': 'Mago'},
-    {'nome': 'Aliado', 'classe': 'Caminhão'},
-    {'nome': 'Inimigo', 'classe': 'Mago'},
-    {'nome': 'Principal', 'classe': 'Caminhão'},
-    {'nome': 'Vilão', 'classe': 'Mago'}
-]
+# data.py
 
+# Dicionário de personagens
+personagens = {
+    1: {
+        'nome': 'Markin',
+        'classe': 'Padrão',
+        'nivel': 10,
+        'vida': 100,
+        'ataque': 20,
+        'imagem': 'https://media.tenor.com/x1eW6Z7pMnIAAAAi/animated-man-running.gif'
+
+    },
+    2: {
+        'nome': 'Jorelei',
+        'classe': 'Nerd',
+        'nivel': 8,
+        'vida': 80,
+        'ataque': 25,
+        'imagem': 'https://media.tenor.com/iPWC0upqq_QAAAAi/animated-man-running.gif'       
+
+    },
+    3: {
+        'nome': 'Romil',
+        'classe': 'Atacante',
+        'nivel': 12,
+        'vida': 90,
+        'ataque': 18,
+        'imagem': 'https://64.media.tumblr.com/efa9c31a801e7c54c62c37289d881c69/tumblr_mrw6ovcjPW1rfjowdo1_500.gif'
+    },
+     4: {
+        'nome': 'Nelly',
+        'classe': 'Millenium',
+        'nivel': 80,
+        'vida': 90,
+        'ataque': 98,
+        'imagem': 'https://cdna.artstation.com/p/assets/images/images/044/949/056/original/pedro-manuel-correia-pereira-samurai-1-direita-andar-atacar.gif'
+    },
+    5: {
+        'nome': 'Tofu',
+        'classe': 'Samurai',
+        'nivel': 80,
+        'vida': 90,
+        'ataque': 98,
+        'imagem': 'https://cdna.artstation.com/p/assets/images/images/044/949/082/original/pedro-manuel-correia-pereira-samurai-2-direita-ataque.gif'
+    },
+    6: {
+        'nome': 'Zumbi',
+        'classe': 'Morto',
+        'nivel': 80,
+        'vida': 90,
+        'ataque': 98,
+        'imagem': 'https://i.pinimg.com/originals/a6/21/0f/a6210fd59c68852a3143ccde924e6cf2.gif'
+    },
+    # Adicione mais personagens aqui, se necessário
+}
